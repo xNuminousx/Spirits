d54a790266cea0e06c056ff8b7134720fbc4fd7f
xNuminousx <tylermcguirt3@gmail.com>
2018-05-27 15:53:17 -0500

    Cancel teleporting with Phase

--- src/me/xnuminousx/spirits/listeners/AbilityListener.java
+++ src/me/xnuminousx/spirits/listeners/AbilityListener.java
@@ -4,6 +4,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerAnimationEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 
 import com.projectkorra.projectkorra.BendingPlayer;
@@ -87,5 +89,22 @@ public class AbilityListener implements Listener {
 			new Orb(player);
 		}
 
+	}
+	
+	@EventHandler
+	public void onTeleport(PlayerTeleportEvent event) {
+		
+		Player player = event.getPlayer();
+		BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
+		
+		if (event.isCancelled() || bPlayer == null) {
+			return;
+		} else if (bPlayer.getBoundAbilityName().equalsIgnoreCase(null)) {
+			return;
+		} else if (bPlayer.getBoundAbilityName().equalsIgnoreCase("Vanish")) {
+			if (event.getCause() == TeleportCause.SPECTATE) {
+				event.setCancelled(true);
+			}
+		}
 	}
 }
