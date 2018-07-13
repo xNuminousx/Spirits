7d64644a5a786077831453fcf291b028a614f533
xNuminousx <tylermcguirt3@gmail.com>
2018-07-13 13:59:22 -0500

    Fixed some Spirit formatting

--- src/me/xnuminousx/spirits/elements/SpiritElement.java
+++ src/me/xnuminousx/spirits/elements/SpiritElement.java
@@ -6,7 +6,7 @@ import com.projectkorra.projectkorra.ProjectKorra;
 
 public class SpiritElement {
 	
-	public static final Element SPIRIT = new Element("Spirit", ElementType.BENDING, ProjectKorra.plugin);
-	public static final Element LIGHT_SPIRIT = new Element("LightSpirit", ElementType.BENDING, ProjectKorra.plugin);
-	public static final Element DARK_SPIRIT = new Element("DarkSpirit", ElementType.BENDING, ProjectKorra.plugin);
+	public static final Element SPIRIT = new Element("Spirit", null, ProjectKorra.plugin);
+	public static final Element LIGHT_SPIRIT = new Element("LightSpirit", null, ProjectKorra.plugin);
+	public static final Element DARK_SPIRIT = new Element("DarkSpirit", null, ProjectKorra.plugin);
 }
