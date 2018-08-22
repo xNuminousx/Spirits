83836d599e6d09b92fd88452daf2c5865a334482
xNuminousx <xnuminousx@gmail.com>
2018-08-21 22:35:41 -0500

    Removed an unused import

--- src/me/xnuminousx/spirits/elements/SpiritElement.java
+++ src/me/xnuminousx/spirits/elements/SpiritElement.java
@@ -1,7 +1,6 @@
 package me.xnuminousx.spirits.elements;
 
 import com.projectkorra.projectkorra.Element;
-import com.projectkorra.projectkorra.Element.ElementType;
 import com.projectkorra.projectkorra.ProjectKorra;
 
 public class SpiritElement {
